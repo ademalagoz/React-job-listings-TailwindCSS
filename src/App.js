5e72c4748ff6c9f55beeb08819082e7cc67e6174
unknown <ademalagoz46@gmail.com>
2021-08-18 04:12:42 +0100

    All Completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
 import "./App.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import data from "./data.json";
-// import Home from "./Home";
 import Job from "./components/Job";
 import { useState, useEffect } from "react";
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => {
     setJobs(data);
   }, []);
-  console.log(jobs);
+
+  const handleTagClick = (tag) => {
+    if (filters.includes(tag)) return;
+
+    setFilters([...filters, tag]);
+  };
+
+  const handleFilterClick = (elem) => {
+    setFilters(filters.filter((f) => f !== elem));
+  };
+  const handleClear = () => {
+    setFilters([]);
+  };
 
   return (
-    <div className="App">
-      <header className="bg-teal-500 mb-12">
+    <>
+      <header className="bg-green-600 mb-12">
         <img src="/images/bg-header-desktop.svg" alt="bg-imag" />
       </header>
-      {jobs.map((job) => (
-        <Job job={job} key={job.id} />
-      ))}
-    </div>
+      <div className="container m-auto">
+        <div className=" flex-col bg-white shadow-md my-16 mx-10 p-6 rounded">
+          {filters.length > 0 &&
+            filters.map((filter) => (
+              <span
+                onClick={() => handleFilterClick(filter)}
+                className="cursor-pointer text-blue-400 bg-blue-100 font-bold mr-4 mb-4 p-2 rounded "
+              >
+                <span className="text-blue-400 bg-blue-100 p-2">{filter}</span>
+                <span className="text-blue-400 bg-blue-100 text-xl">X</span>
+              </span>
+            ))}
+
+          <button
+            onClick={handleClear}
+            className="font-bold text-blue-400 mx-auto"
+          >
+            Clear
+          </button>
+        </div>
+
+        {jobs.map((job) => (
+          <Job job={job} key={job.id} handleTagClick={handleTagClick} />
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/components/Job.js
+++ src/components/Job.js
@@ -1,47 +1,72 @@
 import React from "react";
 
-const Job = ({ job }) => {
+const Job = ({ job, handleTagClick }) => {
   const {
     logo,
+    isNew,
+    featured,
     company,
     position,
+    role,
+    level,
     postedAt,
     contract,
     location,
     languages,
     tools,
   } = job;
-  // const langAndTools=[];
-  // if(tools){
-  //   langAndTools.push(...tools)
-  // }
-  // if(languages){
-  //   langAndTools.push(...languages)
-  // }
+
+  const tags = [role, level];
+  if (tools) {
+    tags.push(...tools);
+  }
+  if (languages) {
+    tags.push(...languages);
+  }
   return (
-    <div className="flex bg-white shadow-md m-4 p-6">
+    <div
+      className={`flex flex-col bg-white shadow-md my-16 mx-10 p-6 rounded ${
+        featured && "border-l-4 border-blue-500 "
+      }sm:flex-row`}
+    >
       <div>
-        <img src={logo} alt={company} />
+        <img
+          className="-mt-16 mb-4 w-20 h-20 sm:mt-0 sm:h-24 sm:w-24"
+          src={logo}
+          alt={company}
+        />
       </div>
       <div className="flex flex-col justify-between ml-4">
-        <h3 className="font-bold text-teal-500">{company}</h3>
-        <h2 className="font-bold text-xl">{position}</h2>
+        <h3 className="font-bold text-blue-400">
+          <span className="text-blue-400">{company}</span>
+          {isNew && (
+            <span
+              className="
+            bg-blue-300 text-white font-bold p-2 m-2 py-1 px-2 rounded-full uppercase"
+            >
+              new!
+            </span>
+          )}
+          {featured && (
+            <span className="bg-gray-800 text-white font-bold py-1 px-2 rounded-full uppercase">
+              featured
+            </span>
+          )}
+        </h3>
+
+        <h2 className="font-bold text-xl my-2">{position}</h2>
         <p className="text-gray-700">
           {postedAt} · {contract} · {location}
         </p>
       </div>
-      <div className="flex items-center justify-end ml-auto">
-        {languages
-          ? languages.map((language) => (
-              <span className=" text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded">
-                {language}
-              </span>
-            ))
-          : ""}
-        {tools
-          ? tools.map((tool) => (
-              <span className="text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded">
-                {tool}
+      <div className="flex flex-wrap items-center mt-4 pt-4 border-t border-gray-500 border-solid sm:ml-auto sm:border-0 sm:pt-0 ">
+        {tags
+          ? tags.map((tag) => (
+              <span
+                onClick={() => handleTagClick(tag)}
+                className=" cursor-pointer text-blue-400 bg-blue-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0"
+              >
+                {tag}
               </span>
             ))
           : ""}
